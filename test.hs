680f42e620ff4095410f0556534efd15af3cccd9
sniddare <simon.simon.nilsson@gmail.com>
2014-03-19 18:40:30 +0100

    testa test

--- test.hs
+++ test.hs
@@ -2,3 +2,6 @@ import T9
 import Messages
 
 a = [ str ++ "\n" ++ get_t9_sequence str ++ "\n\n" | str <- messages ]
+
+--Prints the output made to 'a' and writes it to 'test.txt'.
+-- writeFile "test.txt" $ unwords a
